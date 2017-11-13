970a2d78e131e0c8b291aeb14b5f51f4fe0000dc
LatsynNiK <latsynn@mail.ru>
2017-11-13 20:56:12 +0300

    Установка и создание прроекта на django cms

--- points_base.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import points_class
-
-conn = sqlite3.connect('points.sqlite')
-c = conn.cursor()
-
-def add_user(identification_object,adress,geolocation,date):
-    c.execute("INSERT INTO points (identification_object,adress,geolocation,date) VALUES (?,?,?,?)",(identification_object,adress,geolocation,date))
-    conn.commit()
