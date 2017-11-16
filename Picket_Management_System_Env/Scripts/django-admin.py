f3ee63c24b6b1f1233f48708700592132347d7d6
LatsynNiK <latsynn@mail.ru>
2017-11-17 01:26:29 +0300

    Исправление структуры проекта
    вынес виртуальное окружение, так как оно не нужно в самом проекте и
    вообще должно устанавливаться для проекта локально (так как использует
    абсолютные пути)

--- Picket_Management_System_Env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\николай\desktop\иу5\picket_management_system\picket_management_system_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
