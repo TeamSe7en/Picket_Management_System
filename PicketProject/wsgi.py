128ae4d2ee539595a287da742832f69ff797b1da
Latsyn Nikolay <latsynn@mail.ru>
2017-11-28 01:10:46 +0300

    Правка файла wsgi

--- PicketProject/wsgi.py
+++ PicketProject/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for PicketProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = os.path.expanduser('~/Picket_Management_System')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'PicketProject.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PicketProject.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
