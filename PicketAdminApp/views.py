bf93878f94d0717f0984edf0fbebf153506ddcb9
LatsynNiK <latsynn@mail.ru>
2017-12-19 02:41:05 +0300

    Фронт главной стр

--- PicketAdminApp/views.py
+++ PicketAdminApp/views.py
@@ -10,7 +10,7 @@ def post_list(request):
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Picket.objects.all().order_by('date')
     print(posts)
-    return render(request, 'PicketAdminApp/post_list.html',{'posts': posts})
+    return render(request, 'PicketAdminApp/index_page.html',{'posts': posts})
 
 def check_task(request):
     task = Task.objects.filter(status=True).first()
